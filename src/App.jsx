7eb39fee554dcaf24a23ea483744071878cff7c3
juanseg8 <jguilisasti503@gmail.com>
2023-09-15 18:23:22 -0300

    singin

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import SingIn from './components/singin'
+import "bootstrap"
+import "react-bootstrap"
+
+function App() {
+
+  return (
+    <>
+      <SingIn />
+    </>
+  )
+}
+
+export default App
